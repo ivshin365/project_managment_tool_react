dfabc5524f08c73e5de8f0d958f3ad02e7a8f0f0
Dimitry <ivshin365@gmail.com>
2023-05-07 13:46:20 +0300

    Addcomponent

--- pages/library.jsx
+++ pages/library.jsx
@@ -42,7 +42,7 @@ export default function Library({ comp }) {
     }
 
     async function newComponent() {
-        router.push(`http://localhost:3000/addcomponent`)
+        router.push(`http://localhost:3000/Addcomponent`)
     }
 
     return (
@@ -55,7 +55,7 @@ export default function Library({ comp }) {
                         Component Library
                     </h2>
                     <a onClick={() => newComponent()}
-                        href="/addcomponent"
+                        href="/Addcomponent"
                         className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Add New Record
